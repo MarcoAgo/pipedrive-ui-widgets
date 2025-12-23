d2cb97f0ee3cb978fb90fb1ef024bc9ad32827a5
Marco Agostinelli <carmelobig@icloud.com>
2025-12-23 11:03:12 +0100

    restore

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,47 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 import "./App.css";
 
+import AppExtensionsSDK from "@pipedrive/app-extensions-sdk";
+
 function App() {
+  const [count, setCount] = useState(0);
+
+  async function initPipedrive() {
+    // If your URL already includes ?id=..., SDK finds it automatically
+    const sdk = await new AppExtensionsSDK().initialize();
+
+    // Now you can use SDK features (e.g., send commands, resize, show snackbar)
+    console.log("Pipedrive SDK ready!", sdk);
+  }
+
+  useEffect(() => {
+    initPipedrive();
+  }, []);
+
   return (
     <>
-      <p>Hello World</p>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   );
 }
